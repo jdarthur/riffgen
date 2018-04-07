75ef8eab01bd0cc4035ba0e37c352bf815604ed4
jd arthur <jdarthur@gatech.edu>
2018-04-07 19:58:53 -0400

    updated test file to use measure

--- test.py
+++ test.py
@@ -1,15 +1,27 @@
 from classes.note import Note
 from classes.scale import Scale
+from classes.measure import Measure
 
-x = Note(32, 3)
+s = Scale(base_frequency=65.4064)
+
+x = Note(32, 3, scale=s)
 note, duration = x.to_human_readable()
 print(note) #G#(2)
 print(duration) #quarter
 
-y = Note(tone="g#", octave=2, notelength="quarter")
+y = Note(tone="g#", octave=2, notelength="quarter", scale=s)
 note, duration = y.to_human_readable()
-print(note) #G#(2)
-print(duration) #quarter
+print(note)
+print(duration)
 
-s = Scale(base_frequency=65.4064)
 print(s.get_frequency(y.abs_pitch))
+print(x.frequency)
+
+G = Note(tone="g", octave=2, notelength="quarter")
+A = Note(tone="a", octave=2, notelength="eighth")
+m = Measure()
+m.add(G, 0)
+m.add(A, 8)
+print(m.note_dict)
+
+
