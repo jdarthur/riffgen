9f3a0d9d5f74f8bc00c809342ffa3eecce2861e6
jd arthur <jdarthur@gatech.edu>
2018-04-08 21:18:31 -0400

    test2 is simplified

--- test2.py
+++ test2.py
@@ -2,19 +2,12 @@ import numpy as np
 import math
 import time
 import sounddevice as sd
-from bigsample import BigSample
 from classes.note import Note
 from classes.measure import Measure
+from classes.riff import Riff
 from utils import choose_weighted
 import random
 
-
-"""
-global sampling rate
-I'm using 20k because it sounds alright and is faster
-to create arrays than like 44100 or something
-"""
-rate = 20000
 
 """
 ===================================================
@@ -22,22 +15,10 @@ rate = 20000
 ===================================================
 """
 
-def createSample(frequency, length, articulation='regular', rate=rate) :
-	"""
-	Create an array of a musical note from a given
-	note, octave, and length.
-	Articulation is used to specify staccato, legato, etc.
-
-	TODO: add a length field like 
-		quarter note, eighth note, sixteenth, etc 
-
-	"""
-	samples = (np.sin(2*np.pi*np.arange(rate*length)*frequency/rate)).astype(np.float32)
-	#samples = np.multiply(samples, ampl_arr)
-
-	return samples
-
 def random_measure(notecount) :
+	"""
+	create a bunch of random notes in random locations
+	"""
 	measure = Measure()
 	tonechoices = ['c', 'd', 'e', 'f', 'g', 'a', 'b']
 	toneweights = [ 30,  10,  30,  10,  30,  10,  10]
@@ -54,30 +35,20 @@ def random_measure(notecount) :
 		measure.add(note, position)
 	return measure
 
-def add_to_bigsample(measure, bigsample) :
-	samples = []
-	for key in m.note_dict :
-		freq = m.note_dict[key][0]
-		length = m.note_dict[key][1]
-		tup = (createSample(freq, length), key)
-		samples.append(tup)
-
-	#add samples to the master sample
-	for samp, pos in samples :
-		big.add_inplace(samp, pos, m.MAX_POSITION + 1)
 
 #timing. using this to evaluate performance
 timer = time.time()
 
 m = random_measure(12)
+m2 = random_measure(12)
 
-big = BigSample(m.beats, rate)
-add_to_bigsample(m, big)
+r = Riff(Measures=[m, m2])
+big = r.create_sample()
+bytes1 = big.get_bytes()
 
 #second timer to calculate elapsed time
 t2 = time.time() - timer
 print("runtime: {0}s".format(t2))
 
 #actually play our sample
-bytes1 = big.get_bytes()
-sd.play(bytes1, rate, blocking=True)
+sd.play(bytes1, big.rate, blocking=True)
