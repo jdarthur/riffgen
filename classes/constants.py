153f724c53f6c42c8c7c0dc5c8699177f1325adf
jd arthur <jdarthur@gatech.edu>
2018-04-18 22:16:40 -0400

    ran pylint on constants.py

--- classes/constants.py
+++ classes/constants.py
@@ -1,13 +1,22 @@
+"""
+Shared constants
+
+author: John Arthur
+date: 7 April 2018
+"""
+
 duration_dict = {'thirty-second' : 0.03125,
-				 'sixteenth' : 0.0625,
-				 'eighth' : 0.125,
-				 'quarter' : 0.25,
-				 'half' : 0.5,
-				 'whole' : 1.0}
-lendict = 		{'thirty-second' : 1,
-				 'sixteenth' : 2,
-				 'eighth' : 4,
-				 'quarter' : 8,
-				 'half' : 16,
-				 'whole' : 32}
-duration_list = list(duration_dict.keys())
+                 'sixteenth' : 0.0625,
+                 'eighth' : 0.125,
+                 'quarter' : 0.25,
+                 'half' : 0.5,
+                 'whole' : 1.0}
+
+lendict =       {'thirty-second' : 1,
+                 'sixteenth' : 2,
+                 'eighth' : 4,
+                 'quarter' : 8,
+                 'half' : 16,
+                 'whole' : 32}
+
+duration_list = ['thirty-second', 'sixteenth', 'eighth', 'quarter', 'half', 'whole']
