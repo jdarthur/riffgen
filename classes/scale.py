1db69b32b57fe4eff172f6f43c17ced822fe114a
jd arthur <jdarthur@gatech.edu>
2018-04-20 22:49:33 -0400

    updated scale class to add serial() method (also ran pylint)

--- classes/scale.py
+++ classes/scale.py
@@ -1,37 +1,64 @@
+"""
+Scale module
+
+author: JD Arthur
+date: 10 April 2018
+"""
+
 import math
 
-class Scale :
-	"""
-	A scale is a range of possible pitches.
-	It is used to orient a note such as G#(2) into a specific frequency
+class Scale:
+    """
+    A scale is a range of possible pitches.
+    It is used to orient a note such as G#(2) into a specific frequency
 
-	You can specify a base frequency, a number of semitones, and a number of octaves. 
-	args:
-		base_frequency: frequency in Hz of the lowest note in the scale
-			64 Hz is low C in 'Verdi tuning'. 
-		semitones: number of tones in the  chromatic scale
-			Anything other than 12 will probably sound weird 
-		octaves: number of octaves available to scale
-			5 is roughly what you've got available on a digital keyboard
-	"""
-	def __init__(self, base_frequency=64.0, semitones=12, octaves=5, chromatic=None) :
-		self.freq_list = []
-		self.MIN_PITCH = 0
-		self.MAX_PITCH = semitones * octaves - 1
+    You can specify a base frequency, a number of semitones, and a number of octaves.
+    args:
+        base_frequency: frequency in Hz of the lowest note in the scale
+            64 Hz is low C in 'Verdi tuning'.
+        semitones: number of tones in the  chromatic scale
+            Anything other than 12 will probably sound weird
+        octaves: number of octaves available to scale
+            5 is roughly what you've got available on a digital keyboard
+    """
+    def __init__(self, base_frequency=64.0, semitones=12, octaves=5, chromatic=None):
+        self.base_frequency = base_frequency
+        self.semitones = semitones
+        self.octaves = octaves
 
-		if(chromatic ==  None) :
-			self.chromatic = ['c','c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b']
-		else :
-			self.chromatic = chromatic
+        self.freq_list = []
+        self.min_pitch = 0
+        self.max_pitch = semitones * octaves - 1
 
-		for i in range(0, octaves * semitones) :
-			freq = math.pow(2, float(i) / float(semitones)) * base_frequency
-			self.freq_list.append(freq)
+        if chromatic is None:
+            self.chromatic = ['c', 'c#', 'd', 'd#', 'e', 'f',
+                              'f#', 'g', 'g#', 'a', 'a#', 'b']
+        else:
+            self.chromatic = chromatic
 
-	def get_frequency(self, abs_pitch) :
-		return self.freq_list[abs_pitch]
+        for i in range(0, octaves * semitones):
+            freq = math.pow(2, float(i) / float(semitones)) * base_frequency
+            self.freq_list.append(freq)
 
-	
+    def get_frequency(self, abs_pitch):
+        """
+        Get frequency from abs pitch.
+
+        Useful for Note objects
+    	"""
+        return self.freq_list[abs_pitch]
+
+    def serial(self):
+        """
+        return a minimal version of this object that can be used to reconstruct it
+        """
+        minimal = {
+            "base_frequency" : self.base_frequency,
+            "semitones" : self.semitones,
+            "octaves" : self.octaves,
+            "chromatic" : self.chromatic
+        }
+        return minimal
 
 """
 s = Scale(base_frequency=65.4064)
