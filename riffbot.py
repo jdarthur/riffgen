34c20e55dde9aa83183506afa04d31a27fe3d743
jd arthur <jdarthur@gatech.edu>
2018-04-30 21:36:13 -0400

    small gui to replay a riff or create a new one

--- riffbot.py
+++ riffbot.py
@@ -4,11 +4,14 @@ Main class to generate a riff
 
 import time
 import sounddevice as sd
+
+
+from tkinter import *
 from classes.note import Note
 from classes.measure import Measure
 from classes.riff import Riff
 from classes.scale import Scale
-from lib.utils import choose_weighted
+from lib.utils import choose_weighted, consonance
 
 
 """
@@ -48,26 +51,64 @@ def random_measure(notecount, scale=None, subscale=[0, 2, 4, 5, 7, 9, 11], tonew
             measure.add(note, pos)
     return measure
 
-#timing. using this to evaluate performance
-timer = time.time()
+class Window(Frame):
 
-#m = random_measure(12)
-#m2 = random_measure(12)
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.create_elements()
+        self.riff = None
+        self.rate = 0
 
-scale = Scale(semitones=7, chromatic=['a', 'b', 'c', 'd', 'e', 'f'])
-subscale = scale.sub_scale([0, 2, 4, 6])
-m = random_measure(12, scale=scale)
-m2 = random_measure(12, scale=scale)
+    def create_elements(self):
+        """
+        display buttons on the window
+        """
+        self.master.title("riffbot")
+        self.pack(fill=BOTH, expand=1)
 
-r = Riff(measures=[m, m2])
-#r = Riff(filename='test.riff')
-#r.write_riff("test.riff")
-big = r.create_sample()
-bytes1 = big.get_bytes()
+        createbutton = Button(self, text="create", command=self.create_riff)
+        createbutton.place(relx=0.5, rely=0.33, anchor=CENTER)
+        playbutton = Button(self, text="play", command=self.play)
+        playbutton.place(relx=0.5, rely=0.66, anchor=CENTER)
 
-#second timer to calculate elapsed time
-t2 = time.time() - timer
-print("runtime: {0}s".format(t2))
+    def create_riff(self):
+        """
+        create a random riff
+        """
+        
+        #timing. using this to evaluate performance
+        timer = time.time()
 
-#actually play our sample
-sd.play(bytes1, big.rate, blocking=True)
+        m = random_measure(12)
+        m2 = random_measure(12)
+
+        #scale = Scale(semitones=13, chromatic=['a', 'b', 'c', 'd', 'e', 'f'])
+        #subscale = scale.sub_scale([0, 2, 4, 6])
+        #m = random_measure(12, scale=scale)
+        #m2 = random_measure(12, scale=scale)
+
+        r = Riff(measures=[m, m2])
+        #r = Riff(filename='test.riff')
+        #r.write_riff("test.riff")
+        big = r.create_sample()
+        self.riff = big.get_bytes()
+        self.rate = big.rate
+
+        #second timer to calculate elapsed time
+        t2 = time.time() - timer
+        print("runtime: {0}s".format(t2))
+
+    def play(self):
+        """
+        play riff if created
+        """
+        if self.riff is not None:
+            sd.play(self.riff, self.rate, blocking=True)
+        else:
+            print("create riff first")
+
+top = Tk()
+top.geometry("200x100")
+app = Window(top)
+app.mainloop()
